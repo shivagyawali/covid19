db343969fbb7136f171db6b13106635e1e2086eb
geekyboyshiva <shivagyawali@github.com>
2020-03-29 17:55:27 +0545

    data

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', setup);
 
 async function setup() {
-    const ctx = document.getElementById('recoverChart').getContext('2d');
-    const recoverData = await getDataRecovered();
-    const recoverChart = new Chart(ctx, {
+    const ctx = document.getElementById('Chart').getContext('2d');
+    const Data = await getData();
+    const Chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: recoverData.country,
+            labels: Data.country,
             datasets: [
                 {
-                    label: 'No. of People Recovered',
-                    data: recoverData.date,
+                    label: 'No. of People Death',
+                    data: deathData.date,
                     fill: false,
                     borderColor: 'rgba(255, 99, 132, 1)',
                     backgroundColor: 'rgba(255, 99, 132, 0.5)',
@@ -22,15 +22,15 @@ async function setup() {
     });
 }
 
-async function getDataRecovered() {
-    const response = await fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv');
+async function getData() {
+    const response = await fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv');
     const data = await response.text();
     const country = [];
     const date = [];
-    const confirm_data = data.split(/\r?\n|\r/);
+    const total_data = data.split(/\r?\n|\r/);
     for (let count = 0; count < 100; count++) {
         for (let i = 0; i < 2; i++) {
-            let rows = confirm_data[count].split(",");
+            let rows = total_data[count].split(",");
             rows.forEach(row => {
                 const cols = rows;
                 country.push(cols[1]);
